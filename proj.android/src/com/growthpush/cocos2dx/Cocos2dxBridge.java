317721c7d212c3896c7c10d01e8c16a9ab328c6f
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-04-25 11:39:49 +0900

    Send callback custom field after Growth Push initialized instance

--- proj.android/src/com/growthpush/cocos2dx/Cocos2dxBridge.java
+++ proj.android/src/com/growthpush/cocos2dx/Cocos2dxBridge.java
@@ -2,18 +2,33 @@ package com.growthpush.cocos2dx;
 
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
+import android.os.Handler;
+
+import com.growthpush.GrowthPush;
 import com.growthpush.bridge.ExternalFrameworkBridge;
 
 public class Cocos2dxBridge extends ExternalFrameworkBridge {
 
 	@Override
 	protected void callbackExternalFramework(final String customFiled) {
+
+		if (GrowthPush.getInstance().getApplicationId() == 0) {
+			new Handler().post(new Runnable() {
+				@Override
+				public void run() {
+					callbackExternalFramework(customFiled);
+				}
+			});
+			return;
+		}
+
 		Cocos2dxGLSurfaceView.getInstance().queueEvent(new Runnable() {
 			@Override
 			public void run() {
 				didOpenRemoteNotification(customFiled);
 			}
 		});
+
 	}
 
 	/*
